7cc1b217e7b7fdb97dac4442efa343a482da1fdc
Guilherme Santos <gui07015@gmail.com>
2020-06-02 18:28:24 -0300

    WIP: introduction

---
+++ server/introduction/server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const users = [
+  "Guilherme",
+  "San",
+  "Ana",
+  "Bea"
+]
+
+app.get('/users', (req, res) => {
+  const search = String(req.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return res.json(filteredUsers);
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const user = users[id];
+
+  return res.json(user);
+});
+
+app.post('/users', (req, res) => {
+  const data = req.body;
+
+  const user = {
+    name: data.name,
+    email: data.email
+  };
+
+  return res.json(user);
+});
+
+
+app.listen(3333);
